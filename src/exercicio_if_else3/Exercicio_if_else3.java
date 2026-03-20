b37b62d85ef6abaaa9918cb21ec7a9c638a9ed3c
José Henrique Santos Alves <josehalves2005@gmail.com>
2026-03-20 14:37:23 -0300

    Commit: if_else_exercise_2

---
+++ src/exercicio_if_else3/Exercicio_if_else3.java
@@ -0,0 +1,37 @@
+package exercicio_if_else3;
+
+import java.util.Scanner;
+
+public class Exercicio_if_else3 {
+
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+     
+    int a = 0;
+    int b = 0;
+    
+    System.out.println("Digite dois numeros em ordem crescente ou descrescente: ");
+    a = sc.nextInt();
+    b = sc.nextInt();
+
+    
+    
+    if(a <= b){
+    
+       if(b % a != 0){ 
+       System.out.println("Nao e multiplo");
+       }
+       else if(b % a == 0){ 
+       System.out.println("Sao multiplo");}            
+}
+    else{
+       if(a % b != 0){ 
+       System.out.println("Nao e multiplo");
+       }
+       else if(a % b == 0){ 
+       System.out.println("Sao multiplo");}            
+}
+  
+}
+}
+
